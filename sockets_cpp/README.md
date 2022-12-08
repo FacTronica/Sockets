d69035789e18cb30cc8d888a5543e4ebd82f8a29
FacTrónica <webmaster@factronica.cl>
2022-12-08 14:12:27 -0300

    Uso de Sockets en C++
    Sockets Servidor Cliente en C++

---
+++ sockets_cpp/README.md
@@ -0,0 +1,6 @@
+# SocketsCpp
+
+<pre><code>
+$ ./servidor 8088 3 <br>
+$ ./cliente 127.0.0.1 8088 mensaje
+</code></pre>
---
+++ sockets_cpp/cliente/cliente.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <signal.h>
+#include "TCPClient.h"
+
+TCPClient tcp;
+
+
+
+
+void sig_exit(int s)
+{
+	tcp.exit();
+	exit(0);
+}
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	if(argc != 4) {
+		cerr << "MODO DE USO: ./cliente ip puerto mensaje" << endl;
+		return 0;
+	}
+	signal(SIGINT, sig_exit);
+
+	tcp.setup(argv[1],atoi(argv[2]));
+	while(1)
+	{
+		tcp.Send(argv[3]);
+		string rec = tcp.receive();
+		if( rec != "" )
+		{
+			cout << rec << endl;
+		}
+		sleep(1);
+	}
+	return 0;
+}
